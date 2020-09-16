bc26a45d73efcd345be574079f5911b532688121
MyUsernameIsJoker <47589295+MyUsernameIsJoker@users.noreply.github.com>
2020-09-16 08:01:04 +0800

    node事件环

--- node/eventloop.js
+++ node/eventloop.js
@@ -3,3 +3,11 @@ process.nextTick(()=>{
   console.log('nextTick')
 });
 console.log(1)
+
+Promise.resolve().then(()=>{
+  console.log('promise');
+});
+
+process.nextTick(()=>{
+  console.log('nextTick1')
+})
