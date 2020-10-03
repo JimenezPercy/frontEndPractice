b4898accb621ac463c90a578bf48592ac76c23f4
MyUsernameIsJoker <47589295+MyUsernameIsJoker@users.noreply.github.com>
2020-10-04 07:58:03 +0800

    ajax

--- ajax/server.js
+++ ajax/server.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(__dirname));
 
-app.get('/get',(req,res)=>{
-       res.send('Hello world!')
+app.get('/get', (req, res) => {
+    // res.send('Hello world!');
+    // console.log(req.query);
+    res.send(req.query)
 });
 
-app.get('/resData',(req,res)=>{
-    res.send({"name":"lucy"})
+app.post('/post', (req, res) => {
+    res.send(req.body);
+    console.log(req);
+});
+
+app.get('/resData', (req, res) => {
+    res.send({"name": "lucy"});
 });
 
 app.listen(8888)
