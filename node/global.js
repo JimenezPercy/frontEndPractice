0f7c59f7bff6e4019f81604fda8a58548ba6980d
wangzhipeng <wangzhipeng01@inspur.com>
2020-11-23 20:12:37 +0800

    global

--- node/global.js
+++ node/global.js
@@ -2,19 +2,19 @@
 // console.log(global.process.argv)
 const {program}=require('commander');
 
-// program.name('node');
-// program.usage('1212321global.js');
-// program.option('-p,--port <V>','set port');
-// program.option('-h,--host <V>','set host');
-// program.command('create').action(()=>{
-//   console.log('正在创建项目');
-// });
-// program.on('--help',()=>{
-//   console.log('\r\n Run Command');
-//   console.log('\r\n node node/global.js');
-// });
-// let r=program.parse(process.argv);
-// console.log("============")
-// console.log(r)
+program.name('node');
+program.usage('1212321global.js');
+program.option('-p,--port <V>','set port');
+program.option('-h,--host <V>','set host');
+program.command('create').action(()=>{
+  console.log('正在创建项目');
+});
+program.on('--help',()=>{
+  console.log('\r\n Run Command');
+  console.log('\r\n node node/global.js');
+});
+let r=program.parse(process.argv);
+console.log("============")
+console.log(r)
 
 console.log(process.env);
